dcb07c027c52abf628c4fb7d6cba55d676e12b2a
qinqinqiny <140678008+qinqinqiny@users.noreply.github.com>
2023-07-27 10:57:10 +0800

    up

---
+++ assets/js/addons.js
@@ -0,0 +1,6 @@
+define([], function () {
+    if (Config.modulename === 'index' && Config.controllername === 'user' && ['login', 'register'].indexOf(Config.actionname) > -1 && $("#register-form,#login-form").length > 0 && $(".social-login").length == 0) {
+    $("#register-form,#login-form").append(Config.third.loginhtml || '');
+}
+
+});
